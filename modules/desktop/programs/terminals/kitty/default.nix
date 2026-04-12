5ead8c8ac26dd000a361f20d31bd33561b842df6
lnxll <118633911+lnxll@users.noreply.github.com>
2026-04-12 12:44:15 +0200

    feat(kitty): add copy/paste keybindings

--- modules/desktop/programs/terminals/kitty/default.nix
+++ modules/desktop/programs/terminals/kitty/default.nix
@@ -7,6 +7,16 @@
         touch_scroll_multiplier = "5.0";
         window_padding_width = 20;
       };
+
+      extraConfig = ''
+        map ctrl+c copy_to_clipboard
+        map ctrl+v paste_from_clipboard
+        '' 
+        # support ua/ru kb
+        + ''
+        map ctrl+с copy_to_clipboard
+        map ctrl+м paste_from_clipboard
+      '';
     };
   };
 }
